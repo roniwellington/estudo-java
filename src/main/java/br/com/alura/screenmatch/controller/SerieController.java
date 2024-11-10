197d184e646d08268e2d54fd90bcd0dac42721b3
roniwellington <roniwellington@gmail.com>
2024-11-09 23:04:26 -0300

    Tornando a aplicação para a Web

---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SerieController {
+    @Autowired
+    private SerieRepository repositorio;
+    @GetMapping("/series")
+    public List<Serie> obterSeries(){
+        return repositorio.findAll();
+    }
+}
